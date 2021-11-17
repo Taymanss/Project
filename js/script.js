752fae54e7efd7d4f532775a60daec252cbef6f7
Viacheslav <taymans@yandex.ru>
2021-11-17 09:33:05 +0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-"use strict";
-var number = 5;
-const LeftBorderWidth = 1;
-number = 10;
-console.log(number);
-const obj = {
-    a: 50
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-obj.a = 10;
-console.log(obj);
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
